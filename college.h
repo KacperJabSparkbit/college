53a0a351a5bc884006d6d9eca811beeb13d142e4
mg448246 <117486776+mg448246@users.noreply.github.com>
2024-01-13 22:24:12 +0100

    weak_ptr

--- college.h
+++ college.h
@@ -2,65 +2,68 @@
 // Created by Kacper Jablonski on 24/12/2023.
 //
 
-#ifndef COLLEGE_COLLEGE_H
-#define COLLEGE_COLLEGE_H
-
 #include <string>
 #include <utility>
-#include <vector>
-#include <iostream>
 #include <regex>
 #include <set>
-#include <assert.h> //
-#include <map>
 #include <exception>
 
 class Course;
+
 class Person;
+
 class Student;
+
 class Teacher;
+
 class PhDStudent;
+
 class College;
 
-
-template <typename T>
+template<typename T>
 concept CollegeMemberNonPhD = std::is_same_v<Student, T> || std::is_same_v<Teacher, T>;
 
-template <typename T>
+template<typename T>
 concept CollegeMember = CollegeMemberNonPhD<T> || std::is_same_v<PhDStudent, T>;
 
-template <typename T>
+template<typename T>
 concept PersonType = CollegeMember<T> || std::is_same_v<Person, T>;
 
 
 class PeopleComparator {
 public:
-    bool operator()(const std::shared_ptr<Person>& lhs, const std::shared_ptr<Person>& rhs) const;
+    bool operator()(const std::weak_ptr<Person> &lhs, const std::weak_ptr<Person> &rhs) const;
 };
-template <PersonType T>
+
+template<PersonType T>
 using PeopleCollection = std::set<std::shared_ptr<T>, PeopleComparator>;
+
+template<PersonType T>
+using WeakPeopleCollection = std::set<std::weak_ptr<T>, PeopleComparator>;
 
 class CourseComparator {
 public:
-    bool operator()(const std::shared_ptr<Course>& lhs, const std::shared_ptr<Course>& rhs) const;
+    bool operator()(const std::shared_ptr<Course> &lhs, const std::shared_ptr<Course> &rhs) const;
 };
+
 using CoursesCollection = std::set<std::shared_ptr<Course>, CourseComparator>;
-
-
 
 
 class Course {
 private:
     std::string name;
     bool active = true;
-    College* college;
-    PeopleCollection<Student> students;
-    PeopleCollection<Teacher> teachers;
+    College *college;
+    WeakPeopleCollection<Student> students;
+    WeakPeopleCollection<Teacher> teachers;
+
+    void change_activeness(bool new_active);
 
 public:
     Course(std::string name, bool active = true);
+
     std::string get_name() const;
-    void change_activeness(bool new_active);
+
     bool is_active() const;
 
     friend College;
@@ -72,21 +75,19 @@ private:
     std::string name;
     std::string surname;
 protected:
-    College* college = nullptr;
+    College *college = nullptr;
 
 public:
     Person(std::string name, std::string surname);
+
     std::string get_name() const;
+
     std::string get_surname() const;
+
     virtual ~Person() = default;
 
     friend College;
 };
-
-
-
-
-
 
 
 class Student : virtual public Person {
@@ -98,11 +99,14 @@ protected:
 
 public:
     Student(std::string name, std::string surname, bool active = true);
+
     bool is_active() const;
-    const CoursesCollection& get_courses() const;
+
+    const CoursesCollection &get_courses() const;
 
     friend College;
 };
+
 
 class Teacher : virtual public Person {
 private:
@@ -110,59 +114,71 @@ private:
 
 public:
     Teacher(std::string name, std::string surname);
-    const CoursesCollection& get_courses() const;
+
+    const CoursesCollection &get_courses() const;
 
     friend College;
 };
 
+
 class PhDStudent : public Student, public Teacher {
 public:
     PhDStudent(std::string name, std::string surname, bool active = true);
-    //const CoursesCollection& get_courses() const;
 };
 
 class College {
-    private:
-
+private:
     PeopleCollection<Person> people[3];
     CoursesCollection courses;
 
-    bool person_name_unique(std::shared_ptr<Person> person);
-    bool try_add_person(int cointainer_index, std::shared_ptr<Person> person);
+    bool person_name_unique(const std::shared_ptr<Person> &person);
+
+    bool try_add_person(int cointainer_index, const std::shared_ptr<Person> &person);
 
     template<PersonType T>
-    void append_matching(PeopleCollection<T>& appendable, int containrer_index, std::string name_pattern, std::string surname_pattern);
+    void append_matching(PeopleCollection<T> &appendable, int containrer_index, const std::string &name_pattern,
+                         const std::string &surname_pattern);
 
     template<CollegeMemberNonPhD T>
-    bool assign_course_inner(std::shared_ptr<Person> person, std::shared_ptr<Course> course);
+    bool assign_course_inner(const std::shared_ptr<Person> &person, const std::shared_ptr<Course> &course);
+
+    void check_course(const std::shared_ptr<Course> &course);
+
+    void check_course_existence(const std::shared_ptr<Course> &course);
+
+    void check_course_activeness(const std::shared_ptr<Course> &course);
 
 public:
-    template <CollegeMember T>
-    bool add_person(std::string name, std::string surname, bool active = true);
+    template<CollegeMember T>
+    bool add_person(const std::string &name, const std::string &surname, bool active = true) {
+        if (std::is_same_v<T, Teacher>) {
+            std::shared_ptr<Person> added = std::make_shared<Teacher>(name, surname);
+            return try_add_person(1, added);
+        } else if (std::is_same_v<T, Student>) {
+            std::shared_ptr<Person> added = std::make_shared<Student>(name, surname, active);
+            return try_add_person(0, added);
+        } else {
+            std::shared_ptr<Person> added = std::make_shared<PhDStudent>(name, surname, active);
+            return try_add_person(2, added);
+        }
+    }
 
-    bool add_course(std::string name, bool mandatory = true);
-    
-    
-    template <PersonType T>
-    PeopleCollection<T> find(std::string name, std::string surname);
+    bool add_course(const std::string &name, bool mandatory = true);
 
-    template <CollegeMemberNonPhD T>
-    PeopleCollection<T> find(std::shared_ptr<Course> course);
+    template<PersonType T>
+    PeopleCollection<T> find(const std::string &name, const std::string &surname);
 
-    CoursesCollection find_courses(const std::string& pattern);
+    template<CollegeMemberNonPhD T>
+    WeakPeopleCollection<T> find(const std::shared_ptr<Course> &course);
 
+    CoursesCollection find_courses(const std::string &pattern);
 
-    bool change_course_activeness(std::shared_ptr<Course> course, bool active);
+    bool change_course_activeness(const std::shared_ptr<Course> &course, bool active);
 
     bool remove_course(std::shared_ptr<Course> course);
 
     bool change_student_activeness(std::shared_ptr<Student> student, bool active);
 
-
-    template <CollegeMemberNonPhD T>
-    bool assign_course(std::shared_ptr<T> person, std::shared_ptr<Course> course);
+    template<CollegeMemberNonPhD T>
+    bool assign_course(const std::shared_ptr<T> &person, const std::shared_ptr<Course> &course);
 };
-
-
-
-#endif
