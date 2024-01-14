19d4ff4fbd60a48a871adc7be56bcb955109a478
kacperjablonski <kacper.jablonski@sparkbit.pl>
2024-01-14 16:03:00 +0100

    fix for latest tests

--- college.cc
+++ college.cc
@@ -1,19 +1,21 @@
 #include "college.h"
 #include <limits>
 
-bool PeopleComparator::operator()(const std::shared_ptr<Person>& lhs, const std::shared_ptr<Person>& rhs) const {
-    if (lhs->get_surname() == rhs->get_surname())
-        return lhs->get_name() < rhs->get_name();
-    return lhs->get_surname() < rhs->get_surname();
+bool PeopleComparator::operator()(const std::weak_ptr<Person> &lhs, const std::weak_ptr<Person> &rhs) const {
+    auto slhs = lhs.lock();
+    auto srhs = rhs.lock();
+    if (slhs->get_surname() == srhs->get_surname())
+        return slhs->get_name() < srhs->get_name();
+    return slhs->get_surname() < srhs->get_surname();
 }
 
-bool CourseComparator::operator()(const std::shared_ptr<Course>& lhs, const std::shared_ptr<Course>& rhs) const {
+bool CourseComparator::operator()(const std::shared_ptr<Course> &lhs, const std::shared_ptr<Course> &rhs) const {
     return lhs->get_name() < rhs->get_name();
 }
 
 
 namespace {
-bool wildcardMatch(const std::string& pattern, const std::string& text) {
+bool wildcardMatch(const std::string &pattern, const std::string &text) {
     std::regex star_replace("\\*");
     std::regex questionmark_replace("\\?");
     std::regex regexPattern("([.+^=!:${}()|\\[\\]\\/\\\\])");
@@ -28,14 +30,21 @@ bool wildcardMatch(const std::string& pattern, const std::string& text) {
     return regex_match(text, wildcard_regex);
 }
 
-} //namespace
-
-
+bool find_course_by_name(const CoursesCollection &courses, const std::string &name) {
+    for (const auto &i: courses) {
+        if (i->get_name() == name) {
+            return true;
+        }
+    }
+    return false;
+}
+}
 
 //Course:
 Course::Course(std::string name, bool active) {
     this->name = std::move(name);
     this->active = active;
+    this->college = nullptr;
 }
 
 std::string Course::get_name() const {
@@ -49,8 +58,6 @@ void Course::change_activeness(bool new_active) {
 bool Course::is_active() const {
     return active;
 }
-
-
 
 //Person:
 Person::Person(std::string name, std::string surname) {
@@ -67,8 +74,6 @@ std::string Person::get_surname() const {
     return surname;
 }
 
-
-
 //Student:
 Student::Student(std::string name, std::string surname, bool active) :
         Person(std::move(name), std::move(surname)) {
@@ -79,7 +84,7 @@ bool Student::is_active() const {
     return active;
 }
 
-const CoursesCollection& Student::get_courses() const {
+const CoursesCollection &Student::get_courses() const {
     return courses;
 }
 
@@ -91,24 +96,20 @@ void Student::change_activeness(bool activeness) {
 Teacher::Teacher(std::string name, std::string surname) :
         Person(std::move(name), std::move(surname)) {}
 
-const CoursesCollection& Teacher::get_courses() const {
+const CoursesCollection &Teacher::get_courses() const {
     return courses;
 }
 
-
-
 //PhDStudent:
 PhDStudent::PhDStudent(std::string name, std::string surname, bool active) :
+        Person(name, surname),
         Student(std::move(name), std::move(surname), active),
-        Teacher(std::move(name), std::move(surname)) ,
-        Person(name, surname)
+        Teacher(std::move(name), std::move(surname))
 {};
 
-
-
 //College:
-bool College::person_name_unique(std::shared_ptr<Person> person) {
-    for (int i : {0, 1, 2}) {
+bool College::person_name_unique(const std::shared_ptr<Person> &person) {
+    for (int i: {0, 1, 2}) {
         if (people[i].contains(person)) {
             return false;
         }
@@ -116,8 +117,7 @@ bool College::person_name_unique(std::shared_ptr<Person> person) {
     return true;
 }
 
-
-bool College::try_add_person(int cointainer_index, std::shared_ptr<Person> person) {
+bool College::try_add_person(int cointainer_index, const std::shared_ptr<Person> &person) {
     if (person_name_unique(person)) {
         people[cointainer_index].emplace(person);
         person->college = this;
@@ -127,44 +127,25 @@ bool College::try_add_person(int cointainer_index, std::shared_ptr<Person> perso
     }
 }
 
-template<>
-bool College::add_person<Student>(std::string name, std::string surname, bool activeness) {
-    std::shared_ptr<Person> added = std::make_shared<Student>(name, surname, activeness);
-    return try_add_person(0, added);
-}
-
-template<>
-bool College::add_person<Teacher>(std::string name, std::string surname, bool activeness) {
-    std::shared_ptr<Person> added = std::make_shared<Teacher>(name, surname);
-    return try_add_person(1, added);
-}
-
-template<>
-bool College::add_person<PhDStudent>(std::string name, std::string surname, bool activeness) {
-    std::shared_ptr<Person> added = std::make_shared<PhDStudent>(name, surname, activeness);
-    return try_add_person(2, added);
-}
-
-
-bool College::add_course(std::string name, bool activeness) {
+bool College::add_course(const std::string &name, bool activeness) {
     std::shared_ptr<Course> added = std::make_shared<Course>(name, activeness);
     added->college = this;
-    return courses.emplace(added).second;
+    return courses.insert(added).second;
 }
 
-
 template<PersonType T>
-void College::append_matching(PeopleCollection<T>& appendable, int containrer_index, std::string name_pattern, std::string surname_pattern) {
+void College::append_matching(PeopleCollection<T> &appendable, int containrer_index, const std::string &name_pattern,
+                              const std::string &surname_pattern) {
     if (name_pattern.find_first_of("*?") == std::string::npos &&
-      surname_pattern.find_first_of("*?") == std::string::npos) {
+        surname_pattern.find_first_of("*?") == std::string::npos) {
         auto iter = people[containrer_index].find(std::make_shared<Person>(name_pattern, surname_pattern));
         if (iter != people[containrer_index].end()) {
             appendable.emplace(std::dynamic_pointer_cast<T>(*iter));
         }
         return;
     }
-    
-    for (auto i : people[containrer_index]) {
+
+    for (auto i: people[containrer_index]) {
         if (wildcardMatch(name_pattern, i->get_name()) && wildcardMatch(surname_pattern, i->get_surname())) {
             appendable.emplace(std::dynamic_pointer_cast<T>(i));
         }
@@ -172,39 +153,38 @@ void College::append_matching(PeopleCollection<T>& appendable, int containrer_in
 }
 
 template<>
-PeopleCollection<Person> College::find<Person>(std::string name, std::string surname) {
+PeopleCollection<Person> College::find<Person>(const std::string &name, const std::string &surname) {
     PeopleCollection<Person> matches;
     append_matching(matches, 0, name, surname);
     append_matching(matches, 1, name, surname);
     append_matching(matches, 2, name, surname);
-    return(matches);
+    return (matches);
 }
 
 template<>
-PeopleCollection<Student> College::find<Student>(std::string name, std::string surname) {
+PeopleCollection<Student> College::find<Student>(const std::string &name, const std::string &surname) {
     PeopleCollection<Student> matches;
     append_matching(matches, 0, name, surname);
     append_matching(matches, 2, name, surname);
-    return(matches);
+    return (matches);
 }
 
 template<>
-PeopleCollection<Teacher> College::find<Teacher>(std::string name, std::string surname) {
+PeopleCollection<Teacher> College::find<Teacher>(const std::string &name, const std::string &surname) {
     PeopleCollection<Teacher> matches;
     append_matching(matches, 1, name, surname);
     append_matching(matches, 2, name, surname);
-    return(matches);
+    return (matches);
 }
 
 template<>
-PeopleCollection<PhDStudent> College::find<PhDStudent>(std::string name, std::string surname) {
+PeopleCollection<PhDStudent> College::find<PhDStudent>(const std::string &name, const std::string &surname) {
     PeopleCollection<PhDStudent> matches;
     append_matching(matches, 2, name, surname);
-    return(matches);
+    return (matches);
 }
 
-
-CoursesCollection College::find_courses(const std::string& pattern) {
+CoursesCollection College::find_courses(const std::string &pattern) {
     CoursesCollection matching;
 
     if (pattern.find_first_of("*?") == std::string::npos) {
@@ -215,7 +195,7 @@ CoursesCollection College::find_courses(const std::string& pattern) {
         return matching;
     }
 
-    for (auto i : courses) {
+    for (const auto &i: courses) {
         if (wildcardMatch(pattern, i->get_name())) {
             matching.emplace(i);
         }
@@ -225,22 +205,22 @@ CoursesCollection College::find_courses(const std::string& pattern) {
 
 
 template<>
-PeopleCollection<Student> College::find<Student>(std::shared_ptr<Course> course) {
+WeakPeopleCollection<Student> College::find<Student>(const std::shared_ptr<Course> &course) {
     if (course->college != this) {
-        throw std::runtime_error("Non-existing course.");
+        return WeakPeopleCollection<Student>();
     }
     return course->students;
 }
 
 template<>
-PeopleCollection<Teacher> College::find<Teacher>(std::shared_ptr<Course> course) {
+WeakPeopleCollection<Teacher> College::find<Teacher>(const std::shared_ptr<Course> &course) {
     if (course->college != this) {
-        throw std::runtime_error("Non-existing course.");
+        return WeakPeopleCollection<Teacher>();
     }
     return course->teachers;
 }
 
-bool College::change_course_activeness(std::shared_ptr<Course> course, bool active) {
+bool College::change_course_activeness(const std::shared_ptr<Course> &course, bool active) {
     if (course->college != this) {
         return false;
     }
@@ -268,50 +248,57 @@ bool College::change_student_activeness(std::shared_ptr<Student> student, bool a
 }
 
 template<CollegeMemberNonPhD T>
-bool College::assign_course_inner(std::shared_ptr<Person> person, std::shared_ptr<Course> course) {
+bool College::assign_course_inner(const std::shared_ptr<Person> &person, const std::shared_ptr<Course> &course) {
     if (person->college != this) {
         throw std::runtime_error("Non-existing person.");
     }
+    check_course(course);
+    if constexpr (std::is_same_v<Student, T>) {
+        std::dynamic_pointer_cast<Student>(person);
+        if (!(std::dynamic_pointer_cast<Student>(person))->is_active()) {
+            throw std::runtime_error("Incorrect operation for an inactive student.");
+        }
+        auto studentCourses = std::dynamic_pointer_cast<Student>(person)->get_courses();
+        if (find_course_by_name(studentCourses, course->get_name())) {
+            return false;
+        }
+        std::dynamic_pointer_cast<Student>(person)->courses.insert(course);
+        return course->students.insert(std::dynamic_pointer_cast<Student>(person)).second;
+    } else {
+        auto teacherCourses = std::dynamic_pointer_cast<Teacher>(person)->get_courses();
+        if (find_course_by_name(teacherCourses, course->get_name())) {
+            return false;
+        }
+        std::dynamic_pointer_cast<Teacher>(person)->courses.insert(course);
+        return course->teachers.insert(std::dynamic_pointer_cast<Teacher>(person)).second;
+    }
+}
+
+void College::check_course(const std::shared_ptr<Course> &course) {
+    check_course_existence(course);
+    check_course_activeness(course);
+}
+
+void College::check_course_activeness(const std::shared_ptr<Course> &course) {
+    if (!course->is_active()) {
+        throw std::runtime_error("Incorrect operation on an inactive course.");
+    }
+}
+
+void College::check_course_existence(const std::shared_ptr<Course> &course) {
     if (course->college != this) {
-        
         throw std::runtime_error("Non-existing course.");
     }
-    if (!course->is_active()) {
-        throw std::runtime_error("Incorrect operation on an inactive course");
-    }
-    
-    if constexpr (std::is_same_v<Student, T>) {
-        auto t = std::dynamic_pointer_cast<Student>(person);
-        if (!t->is_active()) {
-            throw std::runtime_error("Incorrect operation on an inactive student");
-        }
-        std::dynamic_pointer_cast<Student>(person)->courses.emplace(course);
-        return course->students.emplace(std::dynamic_pointer_cast<Student>(person)).second;
-    } else {
-        std::dynamic_pointer_cast<Teacher>(person)->courses.emplace(course);
-        return course->teachers.emplace(std::dynamic_pointer_cast<Teacher>(person)).second;
-    }
-    return true;
 }
 
 template<>
-bool College::assign_course<Student>(std::shared_ptr<Student> person, std::shared_ptr<Course> course) {
-    if (course->college != this) {
-        throw std::runtime_error("Non-existing course.");
-    }
-    if (!course->is_active()) {
-        throw std::runtime_error("Incorrect operation on an inactive course");
-    }
+bool College::assign_course<Student>(const std::shared_ptr<Student> &person, const std::shared_ptr<Course> &course) {
+    check_course(course);
     return assign_course_inner<Student>(person, course);
 }
 
 template<>
-bool College::assign_course<Teacher>(std::shared_ptr<Teacher> person, std::shared_ptr<Course> course) {
-    if (course->college != this) {
-        throw std::runtime_error("Non-existing course.");
-    }
-    if (!course->is_active()) {
-        throw std::runtime_error("Incorrect operation on an inactive course");
-    }
+bool College::assign_course<Teacher>(const std::shared_ptr<Teacher> &person, const std::shared_ptr<Course> &course) {
+    check_course(course);
     return assign_course_inner<Teacher>(person, course);
 }
